dcc24b4ca3e98ff5ec5571e9292214e6d2138bb4
on3ginnn <124593750+on3ginnn@users.noreply.github.com>
2024-11-07 16:37:27 +0300

    add books

--- src/main/java/com/aptproject/SpringLibraryProject/dbexample/constants/DBConstants.java
+++ src/main/java/com/aptproject/SpringLibraryProject/dbexample/constants/DBConstants.java
@@ -2,8 +2,8 @@ package com.aptproject.SpringLibraryProject.dbexample.constants;
 
 public interface DBConstants {
     String DB_HOST = "localhost";
-    String DB = "local_db2";
-    String USER = "postgres";
-    String PASSWORD = "12345";
+    String DB = "on3gin_db";
+    String USER = "admin";
+    String PASSWORD = "admin";
     String PORT = "5332";
 }
